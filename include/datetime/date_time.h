782db31f5452577d0f21e88edaa554e588c21461
Jonas Carlström <joncar_94@hotmail.com>
2025-08-04 14:29:36 +0200

    changes

--- include/datetime/date_time.h
+++ include/datetime/date_time.h
@@ -1,31 +1,25 @@
 #pragma once
 #include <chrono>
+#include <cmath>
 #include <ctime>
 #include <iomanip>
 #include <sstream>
 #include <string>
-#include <cmath>
+
+using namespace std::chrono;
 
 namespace datetime {
 
 struct time_span {
-    std::chrono::duration<double> _duration;
+    duration<double> _duration;
 
-    static time_span from_seconds(double sec) {
-        return time_span {std::chrono::duration<double>(sec)};
-    }
+    static time_span from_seconds(double sec) { return time_span {duration<double>(sec)}; }
 
-    static time_span from_minutes(double min) {
-        return time_span {std::chrono::duration<double>(min * 60)};
-    }
+    static time_span from_minutes(double min) { return time_span {duration<double>(min * 60)}; }
 
-    static time_span from_hours(double h) {
-        return time_span {std::chrono::duration<double>(h * 3600)};
-    }
+    static time_span from_hours(double h) { return time_span {duration<double>(h * 3600)}; }
 
-    static time_span from_days(double d) {
-        return time_span {std::chrono::duration<double>(d * 86400)};
-    }
+    static time_span from_days(double d) { return time_span {duration<double>(d * 86400)}; }
 
     double total_seconds() const { return _duration.count(); }
 
@@ -35,7 +29,7 @@ struct time_span {
 
     double total_days() const { return _duration.count() / 86400.0; }
 
-        time_span operator+(const time_span& other) const {
+    time_span operator+(const time_span& other) const {
         return time_span {_duration + other._duration};
     }
 
@@ -52,22 +46,61 @@ struct time_span {
     bool operator>(const time_span& other) const { return _duration > other._duration; }
 
     bool operator==(const time_span& other) const { return _duration == other._duration; }
+
+    /*constexpr time_span operator""_ms(double d) {
+
+    }*/
 };
 
-struct date_time {
-    std::chrono::system_clock::time_point _tp;
+class date_time {
+    system_clock::time_point _tp;
 
-    // --- Static factories ---
-    static date_time now() { return date_time {std::chrono::system_clock::now()}; }
+    using days_t    = duration<double, std::ratio<86400>>;
+    using hours_t   = duration<double, std::ratio<3600>>;
+    using minutes_t = duration<double, std::ratio<60>>;
+    using seconds_t = duration<double>;
 
-    static date_time from_ymd(
+    std::time_t get_time() const { return system_clock::to_time_t(_tp); }
+
+    static std::tm get_tm(const std::time_t& tt) { return *std::localtime(&tt); }
+
+    std::tm get_tm() const { return get_tm(get_time()); }
+
+    template<typename TClock, typename TDuration>
+    system_clock::time_point get_tp(time_point<TClock, TDuration>& tp) const {
+        static_assert(std::is_same_v<TClock, system_clock>, "Non matching clock types.");
+        return time_point_cast<system_clock::duration>(tp);
+    }
+
+  public:
+    date_time()
+        : _tp() {};
+
+    date_time(system_clock::time_point tp)
+        : _tp(tp) {}
+
+    template<typename Duration>
+    date_time(
+        time_point<
+            system_clock,
+            Duration> tp
+    )
+        : _tp() {
+        _tp = time_point_cast<system_clock::duration>(tp);
+    }
+
+    date_time(std::time_t time)
+        : _tp(system_clock::from_time_t(time)) {}
+
+    date_time(
         int year,
         int month,
         int day,
         int hour   = 0,
         int minute = 0,
         int second = 0
-    ) {
+    )
+        : _tp() {
         std::tm t      = {};
         t.tm_year      = year - 1900;
         t.tm_mon       = month - 1;
@@ -76,36 +109,84 @@ struct date_time {
         t.tm_min       = minute;
         t.tm_sec       = second;
         std::time_t tt = std::mktime(&t);
-        return date_time {std::chrono::system_clock::from_time_t(tt)};
+        _tp            = system_clock::from_time_t(tt);
     }
 
+    // --- Static factories ---
+    static date_time now() { return date_time {system_clock::now()}; }
+
     std::string to_string(const char* format = "%Y-%m-%d %H:%M:%S") const {
-        std::time_t tt   = std::chrono::system_clock::to_time_t(_tp);
+        std::time_t tt   = system_clock::to_time_t(_tp);
         std::tm local_tm = *std::localtime(&tt);
         std::ostringstream oss;
         oss << std::put_time(&local_tm, format);
         return oss.str();
     }
 
-    date_time add(const time_span& span) const { return date_time {_tp + span._duration}; }
+    date_time add_time(const std::time_t& time) const { return date_time {get_time() + time}; }
 
-    date_time add(double seconds) const {
-        return date_time {_tp + std::chrono::duration<double>(seconds)};
+    date_time add(const time_span& span) const { return date_time {_tp + cast(span)}; }
+
+    date_time add_years(int years) const {
+        auto tm = get_tm();
+        tm.tm_year += years;
+
+        return date_time {std::mktime(&tm)};
     }
+
+    date_time add_months(int months) const {
+        auto tm = get_tm();
+        tm.tm_mon += months;
+
+        return date_time {std::mktime(&tm)};
+    }
+
+    date_time add_days(double days) const { 
+        return date_time {_tp + days_t(days)}; 
+    }
+
+    date_time add_hours(double hrs) const { return date_time {_tp + hours_t(hrs)}; }
+
+    date_time add_minutes(int mins) const { return date_time {_tp + minutes(mins)}; }
+
+    date_time add_seconds(int sec) const { return date_time {_tp + seconds(sec)}; }
 
     time_span operator-(const date_time& other) const {
-        return time_span {std::chrono::duration<double>(_tp - other._tp)};
+        return time_span {duration<double>(_tp - other._tp)};
     }
 
-    date_time operator+(const time_span& span) const { return date_time {_tp + span._duration}; }
+    date_time operator+(const time_span& span) const {
+        auto dur = cast(span);
+        return date_time {_tp + dur};
+    }
 
-    date_time operator-(const time_span& span) const { return date_time {_tp - span._duration}; }
+    date_time operator-(const time_span& span) const {
+        auto dur = cast(span);
+        return date_time {_tp - dur};
+    }
 
     bool operator<(const date_time& other) const { return _tp < other._tp; }
 
     bool operator>(const date_time& other) const { return _tp > other._tp; }
 
     bool operator==(const date_time& other) const { return _tp == other._tp; }
+
+    std::ostream& operator<<(const std::ostream& other) {}
+
+  private:
+    template <typename T>
+    static system_clock::duration cast(const T& value) {
+
+        if constexpr (std::is_arithmetic_v<T>) {
+            return duration_cast<system_clock::duration>(duration<double>(value));
+        } else if constexpr (std::is_base_of_v<time_span, T>) {
+            return duration_cast<system_clock::duration>(value._duration);
+        } else if constexpr (std::is_base_of_v<duration<typename T::rep, typename T::period>, T>) {
+            return duration_cast<system_clock::duration>(value);
+        } else {
+            static_assert(sizeof(T) == 0, "Unsupported type for casting");
+        }
+    }
 };
 
-}
+} // namespace datetime
